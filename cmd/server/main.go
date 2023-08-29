ae877dd11820861b76b610f7d92a91be04613b2e
kerelape <kerelape@gmail.com>
2023-08-29 13:20:14 +0500

    Fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,26 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"encoding/base64"
+	"log"
 	"os"
 
 	"github.com/kerelape/gophkeeper/cmd/server/config"
 	"github.com/kerelape/gophkeeper/internal/server"
+	"github.com/kerelape/gophkeeper/internal/server/identity"
 	"github.com/pior/runnable"
 )
 
 func main() {
+	log.SetPrefix("[GOPHKEEPER] ")
 	var configuration config.Config
 	if err := config.Read(&configuration); err != nil {
-		fmt.Println(configuration.Description())
+		log.Println(configuration.Description())
 		os.Exit(1)
+	}
+	var secret, decodeSecretError = base64.RawStdEncoding.DecodeString(configuration.Token.Secret)
+	if decodeSecretError != nil {
+		log.Fatalf("failed to parse token secret: %s", decodeSecretError.Error())
 	}
 	var gophkeeper = server.Server{
 		RestAddress:  configuration.Rest.Address,
 		RestCertFile: configuration.Rest.CertFile,
 		RestKeyFile:  configuration.Rest.KeyFile,
 
-		Repository: nil, // @todo #16 Implement identity repository.
+		Repository: &identity.PostgresRepository{
+			PasswordEncoding: base64.StdEncoding,
+
+			DSN: configuration.DatabaseDSN,
+
+			TokenSecret:   secret,
+			TokenLifespan: configuration.Token.Lifespan,
+
+			UsernameMinLength: configuration.UsernameMinLength,
+			PasswordMinLength: configuration.PasswordMinLength,
+		},
 	}
 	runnable.Run(&gophkeeper)
 }
