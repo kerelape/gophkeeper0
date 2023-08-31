32b164b27821794acb25dfd3c063894c076e0226
kerelape <kerelape@gmail.com>
2023-08-31 07:02:15 +0500

    Add pieces table

--- internal/server/identity/initialize_postgres.sql
+++ internal/server/identity/initialize_postgres.sql
@@ -3,3 +3,10 @@ CREATE TABLE IF NOT EXISTS identities(
     username TEXT PRIMARY KEY UNIQUE,
     password TEXT
 );
+
+CREATE TABLE IF NOT EXISTS pieces(
+    rid SERIAL PRIMARY KEY UNIQUE,
+    content BYTEA,
+    salt BYTEA,
+    iv BYTEA,
+);
