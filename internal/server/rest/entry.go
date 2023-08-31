77f463d67af0d236dd50d62cebc499c54e3de834
kerelape <kerelape@gmail.com>
2023-08-31 09:57:20 +0500

    Implement piece entry

--- internal/server/rest/entry.go
+++ internal/server/rest/entry.go
@@ -6,14 +6,13 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/kerelape/gophkeeper/internal/server/domain"
 	"github.com/kerelape/gophkeeper/internal/server/rest/login"
+	"github.com/kerelape/gophkeeper/internal/server/rest/piece"
 	"github.com/kerelape/gophkeeper/internal/server/rest/register"
 )
 
 // Entry is the REST api entry.
 //
-// @todo #7 Implement text storage.
 // @todo #7 Implement blob storage.
-// @todo #7 Implement bank card storage.
 type Entry struct {
 	Repository domain.Repository
 }
@@ -27,9 +26,13 @@ func (e *Entry) Route() http.Handler {
 		authentication = login.Entry{
 			Repository: e.Repository,
 		}
+		piece = piece.Entry{
+			Repository: e.Repository,
+		}
 	)
 	var router = chi.NewRouter()
 	router.Mount("/register", registretion.Route())
 	router.Mount("/login", authentication.Route())
+	router.Mount("/piece", piece.Route())
 	return router
 }
