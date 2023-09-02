ce8909924fee22c71fc226e1aafe9153c4c652c6
kerelape <kerelape@gmail.com>
2023-09-02 17:48:40 +0500

    Implement get

--- internal/server/rest/vault/entry.go
+++ internal/server/rest/vault/entry.go
@@ -1,6 +1,9 @@
 package vault
 
 import (
+	"encoding/json"
+	"errors"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -32,9 +35,41 @@ func (e *Entry) Route() http.Handler {
 	return router
 }
 
-// @todo #32 Implement `GET /vault`
 func (e *Entry) get(out http.ResponseWriter, in *http.Request) {
-	panic("unimplemented")
+	var token = in.Header.Get("Authorization")
+	var identity, identityError = e.Gophkeeper.Identity(in.Context(), (gophkeeper.Token)(token))
+	if identityError != nil {
+		var status = http.StatusInternalServerError
+		if errors.Is(identityError, gophkeeper.ErrBadCredential) {
+			status = http.StatusUnauthorized
+		}
+		http.Error(out, http.StatusText(status), status)
+		return
+	}
+
+	var resources, resourcesError = identity.List(in.Context())
+	if resourcesError != nil {
+		var status = http.StatusInternalServerError
+		http.Error(out, http.StatusText(status), status)
+		return
+	}
+
+	var response = make([](map[string]any), len(resources))
+	for _, resource := range resources {
+		response = append(
+			response,
+			map[string]any{
+				"rid":  (int64)(resource.ID),
+				"meta": resource.Meta,
+				"type": (int)(resource.Type),
+			},
+		)
+	}
+
+	out.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(out).Encode(&response); err != nil {
+		log.Printf("failed to write response: %s\n", err.Error())
+	}
 }
 
 // @todo #32 Implement `DELETE /vault/{rid}`
