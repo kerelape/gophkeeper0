1d787b63c3179ac9a6d195c307f496157ec85b13
kerelape <kerelape@gmail.com>
2023-09-02 08:43:54 +0500

    Implement RestoreBlob

--- pkg/gophkeeper/rest_identity.go
+++ pkg/gophkeeper/rest_identity.go
@@ -1,6 +1,7 @@
 package gophkeeper
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"encoding/base64"
@@ -210,10 +211,48 @@ func (i *RestIdentity) StoreBlob(ctx context.Context, blob Blob, password string
 }
 
 // RestoreBlob implements Identity.
-//
-// @todo #31 Implement RestoreBlob on RestIdentity.
 func (i *RestIdentity) RestoreBlob(ctx context.Context, rid ResourceID, password string) (Blob, error) {
-	panic("unimplemented")
+	var endpoint = fmt.Sprintf("%s/blob/%d", i.Server, rid)
+	var request, requestError = http.NewRequestWithContext(
+		ctx,
+		http.MethodGet, endpoint,
+		nil,
+	)
+	if requestError != nil {
+		return Blob{}, requestError
+	}
+	request.Header.Set("Authorization", (string)(i.Token))
+	request.Header.Set("X-Password", password) // @todo Move password to header
+	var response, responseError = i.Client.Do(request)
+	if responseError != nil {
+		return Blob{}, responseError
+	}
+	switch response.StatusCode {
+	case http.StatusOK:
+		var in = bufio.NewReader(response.Body)
+		var meta, metaError = in.ReadString('\n')
+		if metaError != nil {
+			return Blob{}, errors.Join(
+				fmt.Errorf("parse response: %w", metaError),
+				ErrIncompatibleAPI,
+			)
+		}
+		meta = strings.TrimSuffix(meta, "\n")
+		var blob = Blob{
+			Meta:    meta,
+			Content: response.Body,
+		}
+		return blob, nil
+	case http.StatusUnauthorized:
+		return Blob{}, ErrBadCredential
+	case http.StatusInternalServerError:
+		return Blob{}, ErrServerIsDown
+	default:
+		return Blob{}, errors.Join(
+			fmt.Errorf("unexpected response status: %d", response.StatusCode),
+			ErrIncompatibleAPI,
+		)
+	}
 }
 
 // Delete implements Identity.
