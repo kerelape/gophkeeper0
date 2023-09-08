c9dfb7269754649812961cf4a543a59ec2dc54de
kerelape <kerelape@gmail.com>
2023-09-09 00:47:10 +0500

    Refactor

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
+	"net/http"
+
+	"github.com/kerelape/gophkeeper/pkg/gophkeeper"
 
 	"github.com/kerelape/gophkeeper/internal/cli"
-	"github.com/pior/runnable"
 )
 
 func main() {
 	var server = flag.String("s", "", "Gophkeeper address")
 	flag.Parse()
-	if server == nil {
-		log.Fatal("Missing -s flag")
+	if *server == "" {
+		log.Fatal("missing -s flag")
+	}
+
+	var application = cli.CLI{
+		Gophkeeper: &gophkeeper.RestGophkeeper{
+			Server: *server,
+			Client: *http.DefaultClient,
+		},
+	}
+	if err := application.Run(context.Background()); err != nil {
+		log.Fatal(err)
 	}
 }
